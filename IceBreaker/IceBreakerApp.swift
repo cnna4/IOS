5971a3f846137f9184e1f6e77e3ebf101b80af29
Chidi Nna <cv142chidi@gmail.com>
2025-02-05 13:36:11 -0500

    Initial Commit

---
+++ IceBreaker/IceBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceBreakerApp.swift
+//  IceBreaker
+//
+//  Created by Chidi Nna on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct IceBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
